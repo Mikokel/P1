6c453a01c33d96f3b8b03e3f3ed57a9cd45a8e60
Austin Hickman <arhickman73@gmail.com>
2022-04-19 22:12:19 -0400

    Stuff

---
+++ OlympicGamesApp/OlympicGames.Api/OlympicGames.Api/Controllers/CountController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace OlympicGames.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CountController : ControllerBase
+    {
+        //# Times method called with specific request
+        static private int count = 0;//Created only once
+        private readonly ILogger<CountController> _logger;
+
+        public CountController(ILogger<CountController> logger)
+        {
+            this._logger = logger;
+        }
+  
+        // GET: api/<CountController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            count++;
+            _logger.LogInformation("Count.Get executed. Count: {count}", count);
+            return new string[] { count.ToString() };
+        }
+
+        // GET api/<CountController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        // POST api/<CountController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+            throw new NotImplementedException();
+        }
+
+        // PUT api/<CountController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+            throw new NotImplementedException();
+        }
+
+        // DELETE api/<CountController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ OlympicGamesApp/OlympicGames.App/OlympicGames.App/Program.cs
@@ -0,0 +1,14 @@
+﻿using OlympicGames.UI;
+
+namespace OlympicGames.App
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Uri uri = new Uri("https://localhost:7009");
+            IO io = new IO(uri);
+            await io.BeginWebApp();
+        }
+    }
+}
